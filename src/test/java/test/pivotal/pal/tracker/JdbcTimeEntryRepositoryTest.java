4c7c6d7e52cb621d5f0715a1b4d06e1aea427c7b
Joey Sabani <asabani@pivotal.io>
2018-01-22 08:54:48 -0700

    JDBC template sign up

--- src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
+++ src/test/java/test/pivotal/pal/tracker/JdbcTimeEntryRepositoryTest.java
@@ -2,9 +2,9 @@ package test.pivotal.pal.tracker;
 
 
 import com.mysql.cj.jdbc.MysqlDataSource;
-import io.pivotal.pal.tracker.JdbcTimeEntryRepository;
-import io.pivotal.pal.tracker.TimeEntry;
-import io.pivotal.pal.tracker.TimeEntryRepository;
+import io.pivotal.pal.tracker.timesheets.JdbcTimeEntryRepository;
+import io.pivotal.pal.tracker.timesheets.TimeEntry;
+import io.pivotal.pal.tracker.timesheets.TimeEntryRepository;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.jdbc.core.JdbcTemplate;
